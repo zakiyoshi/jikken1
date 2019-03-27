13ec3ba16c1f58099171e7dcdc84818cf5fcd5bb
Zakiyoshi Jouichi <zakiyoshi.jouichi@gmail.com>
2019-03-27 19:52:16 +0900

    5.0.1

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :jikken1 do
   name 'Jikken1 plugin'
   author 'Author name'
   description 'This is a plugin for Redmine'
-  version '5.0.0'
+  version '5.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
